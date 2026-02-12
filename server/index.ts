8835fac17bec147f7a653f371ed333d00496a05f
Ramphoogat <ram.phoogat@gmail.com>
2026-02-12 14:27:20 +0530

    updates the auth routes in the index.ts file

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import authRoutes from "./src/routes/authRoutes";
+import authRoutes from "./src/routes/authRoutes.ts";
 
 dotenv.config();
 
